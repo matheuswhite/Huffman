8ee4991fda2b8c1b5aaaf05b20b2fc7a360f6d87
Aluno IC <alunoic@labg02m29.(none)>
2014-06-03 16:20:44 -0300

    Alteração da classe No( Add metodos e membros ).

--- Dicionario/No.h
+++ Dicionario/No.h
@@ -1,20 +1,65 @@
 #ifndef NO_H
 #define NO_H
 
+#include <iostream>
+
+//Comentar o codigo!!
+
 class No
 {
 private:
     No* left;
     No* right;
-    bool leaf;
-    unsigned char content;
     int frequencia;
+    bool leaf;
+    bool find;
+    std::string code;
+    unsigned char content;
+
 public:
-    No(){}
-    No(unsigned char cont, int  freq, bool l)
+    No() {}
+
+    No(unsigned char cont, int  freq, bool l, const No& esq = 0, const No& dir = 0)
     {
-        frequencia = freq; content = cont; leaf = l;
+        code = 0;
+        find = false;
+        frequencia = freq;
+        content = cont;
+        leaf = l;
     }
+
+    ~No() {}
+
+    void setCode()
+    {
+
+    }
+    std::string getCode()
+    {
+
+    }
+
+    bool getFind()
+    {
+
+    }
+
+    int getFrequencia()
+    {
+
+    }
+
+    unsigned char getContent()
+    {
+
+    }
+
+    bool isLeaf()
+    {
+        return leaf;
+    }
+
+
 };
 
 #endif // NO_H

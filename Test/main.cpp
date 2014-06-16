e7c3f1e4d31f54dbb7c0f456c9e045c150a2b3de
Matheus Tenorio <tenoriomatheus@live.com>
2014-06-15 23:30:00 -0300

    Add pasta Test

---
+++ Test/main.cpp
@@ -0,0 +1,60 @@
+#include "tree.h"
+
+//Tirar o using namespace std
+void readfile(char* path, int freq[])
+{
+    std::ifstream file;
+    int y;
+
+    file.open(path , std::ios::in | std::ios::binary | std::ios::ate);
+
+    if(file.is_open())
+    {
+        int size = file.tellg();
+        file.seekg(0, std::ios::beg);
+
+
+        for(int i=0; i< size; i++)
+        {
+            unsigned char x = file.get();
+            y = x;
+            (freq[y])++;
+        }
+
+
+//        for(int i=0;i<256;i++)
+//        {
+//            std::cout << i << "/"<< freq[i] << " ";
+//        }
+//        std::cout << "\n";
+
+
+    }
+    else
+    {
+        std::cout << "eRRO!" << std::endl;
+
+    }
+
+    file.close();
+}
+
+
+int main()
+{
+
+    int freq[256] = {0};
+    char* caminho;
+    std::string aux, aux2;
+
+    readfile("huffman.txt", freq);
+
+    Tree* huffman = new Tree(freq);
+
+    //huffman->printTree();
+
+    aux2 = huffman->codingNo(huffman->getRoot(), aux, 's');
+    cout << "s/" << aux2 << endl;
+
+    return 0;
+}
